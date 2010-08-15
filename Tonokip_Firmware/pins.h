707c18f39cf6d8330e0e37ef015a682c45e35297
johnnyr <thehdesk@gmail.com>
2010-08-14 21:11:25 -0700

    testing no min endstops

--- Tonokip_Firmware/pins.h
+++ Tonokip_Firmware/pins.h
@@ -200,19 +200,19 @@
 #define X_STEP_PIN         22
 #define X_DIR_PIN          23
 #define X_ENABLE_PIN       24
-#define X_MIN_PIN           2
+#define X_MIN_PIN           -1
 #define X_MAX_PIN           3
 
 #define Y_STEP_PIN         25
 #define Y_DIR_PIN          26
 #define Y_ENABLE_PIN       27
-#define Y_MIN_PIN          18
+#define Y_MIN_PIN          -1
 #define Y_MAX_PIN          19
 
 #define Z_STEP_PIN         28
 #define Z_DIR_PIN          29
 #define Z_ENABLE_PIN       30
-#define Z_MIN_PIN          20
+#define Z_MIN_PIN          -1
 #define Z_MAX_PIN          21
 
 #define E_STEP_PIN          4
